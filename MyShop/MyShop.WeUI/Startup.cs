2190e1793e35e83dbef771d6ccf52f40277ee49b
israabdillah <39734378+israabdillah@users.noreply.github.com>
2019-11-04 15:00:33 +0700

    Initial Solution Setup

---
+++ MyShop/MyShop.WeUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop.WeUI.Startup))]
+namespace MyShop.WeUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
