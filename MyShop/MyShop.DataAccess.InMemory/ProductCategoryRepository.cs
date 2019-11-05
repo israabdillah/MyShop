068e6eee287c84a86bdd42d4dc00d77a6414a6ba
israabdillah <39734378+israabdillah@users.noreply.github.com>
2019-11-05 11:20:37 +0700

    Product Grouping Adding

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+	public class ProductCategoryRepository
+	{
+		ObjectCache cache = MemoryCache.Default;
+		List<ProductCategory> productCategories;
+
+		public ProductCategoryRepository()
+		{
+			productCategories = cache["productCategories"] as List<ProductCategory>;
+			if (productCategories == null)
+			{
+				productCategories = new List<ProductCategory>();
+			}
+		}
+
+		public void Commit()
+		{
+			cache["productCategories"] = productCategories;
+		}
+
+		public void Insert(ProductCategory p)
+		{
+			productCategories.Add(p);
+		}
+
+		public void Update(ProductCategory productCategory)
+		{
+			ProductCategory productCategoryToUpdate = productCategories.Find(p => p.Id == productCategory.Id);
+
+			if (productCategoryToUpdate != null)
+			{
+				productCategoryToUpdate = productCategory;
+			}
+			else
+			{
+				throw new Exception("Product Category Not Found");
+			}
+		}
+
+		public ProductCategory find(string Id)
+		{
+			ProductCategory ProductCategory = productCategories.Find(p => p.Id == p.Id);
+
+			if (ProductCategory != null)
+			{
+				return ProductCategory;
+			}
+			else
+			{
+				throw new Exception("Product Category Not Found");
+			}
+		}
+
+		public IQueryable<ProductCategory> Collection()
+		{
+			return productCategories.AsQueryable();
+		}
+
+		public void Delete(string Id)
+		{
+			ProductCategory productCategoryToDelete = productCategories.Find(p => p.Id == p.Id);
+
+			if (productCategoryToDelete != null)
+			{
+				productCategories.Remove(productCategoryToDelete);
+			}
+			else
+			{
+				throw new Exception("Product Not Found");
+			}
+		}
+	}
+}
--- MyShop/MyShop.WeUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WeUI/Controllers/ProductManagerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
 
 namespace MyShop.WeUI.Controllers
@@ -11,9 +12,11 @@ namespace MyShop.WeUI.Controllers
 	public class ProductManagerController : Controller
 	{
 		ProductRepository context;
+		ProductCategoryRepository productCategories;
 
 		public ProductManagerController() {
 			context = new ProductRepository();
+			productCategories = new ProductCategoryRepository();
 		}
 		// GET: ProductManager
 		public ActionResult Index()
@@ -23,9 +26,13 @@ namespace MyShop.WeUI.Controllers
 		}
 
 		public ActionResult Create() {
-			Product product = new Product();
-			return View(product);
+			ProductManagerViewModel viewModel = new ProductManagerViewModel();
+
+			viewModel.Product = new Product();
+			viewModel.ProductCategories = productCategories.Collection();
+			return View(viewModel);
 		}
+
 		[HttpPost]
 		public ActionResult Create(Product product) {
 			
@@ -48,7 +55,11 @@ namespace MyShop.WeUI.Controllers
 				return HttpNotFound();
 			}
 			else {
-				return View(product);
+				ProductManagerViewModel viewModel = new ProductManagerViewModel();
+				viewModel.Product = product;
+				viewModel.ProductCategories = productCategories.Collection();
+
+				return View(viewModel);
 			}
 		}
 		[HttpPost]
